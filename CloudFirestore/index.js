cfc9e421907904698c736e5d0feb6bc617f55bd6
minung.han <hmu332233@gmail.com>
2018-10-31 01:13:19 +0900

    [Modify] async await 적용

--- CloudFirestore/index.js
+++ CloudFirestore/index.js
@@ -1,8 +1,21 @@
 const db = require('./firebaseInit');
 
 // document 추가 
-const newUserWord = db.collection('user_words').doc();
-newUserWord.set({
-  id: '123',
-  content: '테스트'
-});
+const setUserWord = async ({ id, content }) => {
+  const newUserWord = db.collection('user_words').doc();
+  newUserWord.set({ id, content });
+}
+
+// document 가져오기
+const getUserWords = async () => {
+  const userWordDocs = await db.collection('user_words').get();
+  userWordDocs.forEach(doc => {
+    console.log(doc.id, '=>', doc.data());
+  });
+}
+
+setUserWord({
+  id: '123456',
+  content: '방금 씀'
+})
+getUserWords();
