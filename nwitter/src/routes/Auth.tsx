47294ea323596c31394c5bdc417b5881d8b75d80
minung.han <hmu332233@gmail.com>
2021-12-19 02:20:48 +0900

    2.1 Login Form part One

--- nwitter/src/routes/Auth.tsx
+++ nwitter/src/routes/Auth.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 
 function Auth() {
+  const handleSubmit = (event: React.SyntheticEvent) => {
+    const formData = new FormData(event.target as HTMLFormElement);
+    const { email, password } = Object.fromEntries(formData);
+    event.preventDefault();
+  }
   return (
-    <span>Auth</span>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input name="email" type="text" placeholder="Email" required />
+        <input name="password" type="password" placeholder="Password" required />
+        {/* <input type="submit" value="Log In" /> */}
+        <button type="submit">Log In</button>
+      </form>
+      <div>
+        <button>Continue with Goole</button>
+        <button>Continue with Github</button>
+      </div>
+    </div>
   );
 };
 export default Auth;
